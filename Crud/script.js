d54d57fde2fdf354f4bef4635e43f9160fa8a7d7
Sauraj-mrinal <msauraji@gmail.com>
2023-11-27 10:58:52 +0530

    Add

---
+++ Crud/script.js
@@ -0,0 +1,18 @@
+// console.log("hii");
+// step 1 first we will select the button
+const form  = document.querySelector('form');
+const submit  = document.querySelector('.submit');
+const update = document.querySelector('.update');
+
+// we have selected the buttons now we have to do 
+// first work with submit button 
+
+// so i am thinking the functionality of submit button 
+// when we click on submit button it will store the data or it will we accept the data
+
+// for submit button we need to apply event on submit button
+// and the event will be click 
+
+submit.addEventListener('click', function(){
+    
+})
